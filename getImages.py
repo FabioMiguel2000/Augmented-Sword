b1dd038e85520492bb0b141f3258fda0d469c162
Fab <fabiomiguelchenhuang@gmail.com>
2023-10-05 17:07:44 +0100

    added phototaking functionality

---
+++ getImages.py
@@ -0,0 +1,23 @@
+import cv2
+import os
+
+cameraCapture = cv2.VideoCapture(1)
+num = 1
+
+while True:
+    success, img = cameraCapture.read()
+
+    k = cv2.waitKey(5)
+
+    if k == ord('s'):
+        cv2.imwrite('./img/samples/uncalibrated_image'+ str(num) +'.png', img)
+        print('Image was successfully saved!')
+        num += 1
+    if k == ord('q'):
+        break
+
+    cv2.imshow('Img', img)
+
+cameraCapture.release()
+
+cv2.destroyAllWindows()
