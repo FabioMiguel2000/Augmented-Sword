701fcd43166fb62984252fc91430f4f156c6100b
Fab <fabiomiguelchenhuang@gmail.com>
2023-10-06 15:43:31 +0100

    remove training images when taking new pictures

--- src/calibration/getImages.py
+++ src/calibration/getImages.py
@@ -1,22 +1,40 @@
 import cv2
+import os
 
-cameraCapture = cv2.VideoCapture(0)
-num = 1
+img_dir = '../../img/samples/training/'
 
-while True:
-    success, img = cameraCapture.read()
+def delete_files_in_directory(directory_path):
+   try:
+     with os.scandir(directory_path) as entries:
+       for entry in entries:
+         if entry.is_file():
+            os.unlink(entry.path)
+     print("All files deleted successfully.")
+   except OSError:
+     print("Error occurred while deleting files.")
 
-    k = cv2.waitKey(5)
+def takePictures():
+    num = 1
+    cameraCapture = cv2.VideoCapture(0)
+    delete_files_in_directory(img_dir)
 
-    if k == ord('s'):
-        cv2.imwrite('../../img/samples/training/uncalibrated_image'+ str(num) +'.png', img)
-        print('Image was successfully saved!')
-        num += 1
-    if k == ord('q'):
-        break
+    while True:
 
-    cv2.imshow('Img', img)
+        success, img = cameraCapture.read()
 
-cameraCapture.release()
+        k = cv2.waitKey(5)
 
-cv2.destroyAllWindows()
+        if k == ord('s'):
+            cv2.imwrite(img_dir + 'uncalibrated_image'+ str(num) +'.png', img)
+            print('Image was successfully saved!')
+            num += 1
+        if k == ord('q'):
+            break
+
+        cv2.imshow('Img', img)
+
+    cameraCapture.release()
+
+    cv2.destroyAllWindows()
+
+takePictures()
